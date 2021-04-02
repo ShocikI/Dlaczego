4d21b880fc66cc9a4e1cf19a9ae41b463fdce934
ShocikI <szotmicha16@gmail.com>
2021-04-02 20:42:08 +0200

    database added

---
+++ dlaczego/config.php
@@ -0,0 +1,6 @@
+<?php
+
+const USERNAME = 'eynhdwelafpics';
+const PASSWORD = '5598739a36fd12716922830f6948a078485156987c8f01bc603b694d56456500';
+const HOST = 'ec2-52-50-171-4.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'd77ehqq1fiuvro';
